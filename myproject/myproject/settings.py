53beb5977dde4382fa35da97b26271e29d81be22
[Olena Karpenko] <[lena.k.21.02@gmail.com]>
2023-06-03 18:10:25 +0200

    Miscellaneous

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'catalog.urls'
+ROOT_URLCONF = 'myproject.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'catalog.wsgi.application'
+WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
