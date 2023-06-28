b0183e9f0d0b9a6bd23d99f75c8a1f272df19252
[Olena Karpenko] <[lena.k.21.02@gmail.com]>
2023-06-28 18:55:06 +0200

    Triangle

--- triangle/views.py
+++ triangle/views.py
@@ -4,9 +4,18 @@ from django.shortcuts import render
 
 
 def triangle_view(request):
-    if request.method == 'POST':
-        cathetus1 = float(request.POST.get('cathetus1'))
-        cathetus2 = float(request.POST.get('cathetus2'))
+    if request.method == 'GET':
+        return render(request, 'form.html')
+    elif request.method == 'POST':
+        cathetus1 = request.POST.get('cathetus1')
+        cathetus2 = request.POST.get('cathetus2')
+
+        try:
+            cathetus1 = float(cathetus1)
+            cathetus2 = float(cathetus2)
+        except ValueError:
+            error_message = "Invalid cathetus values provided."
+            return render(request, 'form.html', {'error_message': error_message})
 
         if cathetus1 <= 0 or cathetus2 <= 0:
             error_message = "Cathetus values should be greater than 0."
@@ -14,7 +23,6 @@ def triangle_view(request):
 
         hypotenuse = sqrt(cathetus1 ** 2 + cathetus2 ** 2)
         return render(request, 'result.html', {'hypotenuse': hypotenuse})
-    else:
-        return render(request, 'form.html', {'error_message': None})
+
 
 # Create your views here.
